e5f71ef169f9f2b78680242afb1cf3da709217cf
romainD <romain.deschamps61@sfr.fr>
2025-01-18 02:39:14 +0100

    Add project files.

---
+++ Patches.cs
@@ -0,0 +1,228 @@
+﻿using Il2Cpp;
+using HarmonyLib;
+
+namespace AllRandomLayoutsAtOnce
+{
+    internal class Patches
+    {
+
+        [HarmonyPatch(typeof(DisableObjectForXPMode), nameof(DisableObjectForXPMode.ShouldDisableForCurrentMode))]
+        internal class DisableObjectForXPMode_ShouldDisableForCurrentMode
+        {
+            private static void Postfix(DisableObjectForXPMode __instance, ref bool __result)
+            {
+                // ---------------   3 BUILDINGS IN COASTAL HIGHWAY DISABLED IN INTERLOPER/MISERY   -----------------
+                if (__result == true)
+                {
+                    if (__instance.name.Contains("STRSPAWN") && Settings.settings.buildingEnabled)
+                    {
+                        __result = false;
+                    }
+                }
+            }
+
+        }
+
+        [HarmonyPatch(typeof(RandomSpawnObject), nameof(RandomSpawnObject.ActivateRandomObject))]
+        internal class RandomSpawnObject_ActivateRandomObject
+        {
+            internal static bool Prefix(RandomSpawnObject __instance)
+            {
+
+
+                if (__instance.m_RerollAfterGameHours == 0)
+                {
+                    string regionName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+
+                    // ---------------   ALL BUILDINGS AND ALL POSSIBLE DOORS FOR FISHING HUTS  -----------------
+
+                    if (__instance.name == "RandomSpawnObjectBase_burnt_nightmare" && Settings.settings.buildingEnabled) return false;
+
+                    if (regionName == "MountainTownRegion" && __instance.transform.GetParent().name == "Houses_Random" && Settings.settings.buildingEnabled)
+                    {
+                        __instance.m_ObjectList[0].SetActive(true);
+                        __instance.m_ObjectList[1].SetActive(true);
+                        __instance.m_ObjectList[2].SetActive(true);
+
+                        __instance.m_ObjectList[0].transform.GetChild(2).gameObject.SetActive(false);
+                        __instance.m_ObjectList[0].transform.GetChild(4).gameObject.SetActive(false);
+                        __instance.m_ObjectList[0].transform.GetChild(9).gameObject.SetActive(false);
+                        __instance.m_ObjectList[0].transform.GetChild(11).gameObject.SetActive(false);
+                        __instance.m_ObjectList[0].transform.GetChild(13).gameObject.SetActive(false);
+                        __instance.m_ObjectList[0].transform.GetChild(15).gameObject.SetActive(false);
+                        __instance.m_ObjectList[0].transform.GetChild(16).gameObject.SetActive(false);
+                        __instance.m_ObjectList[0].transform.GetChild(17).gameObject.SetActive(false);
+                        __instance.m_ObjectList[0].transform.GetChild(18).gameObject.SetActive(false);
+                        __instance.m_ObjectList[0].transform.GetChild(19).gameObject.SetActive(false);
+                        __instance.m_ObjectList[0].transform.GetChild(20).gameObject.SetActive(false);
+                        __instance.m_ObjectList[0].transform.GetChild(21).gameObject.SetActive(false);
+
+                        __instance.m_ObjectList[1].transform.GetChild(0).gameObject.SetActive(false);
+                        __instance.m_ObjectList[1].transform.GetChild(3).gameObject.SetActive(false);
+                        __instance.m_ObjectList[1].transform.GetChild(5).gameObject.SetActive(false);
+                        __instance.m_ObjectList[1].transform.GetChild(7).gameObject.SetActive(false);
+                        __instance.m_ObjectList[1].transform.GetChild(8).gameObject.SetActive(false);
+                        __instance.m_ObjectList[1].transform.GetChild(10).gameObject.SetActive(false);
+                        __instance.m_ObjectList[1].transform.GetChild(12).gameObject.SetActive(false);
+                        __instance.m_ObjectList[1].transform.GetChild(13).gameObject.SetActive(false);
+                        __instance.m_ObjectList[1].transform.GetChild(14).gameObject.SetActive(false);
+                        __instance.m_ObjectList[1].transform.GetChild(17).gameObject.SetActive(false);
+                        __instance.m_ObjectList[1].transform.GetChild(18).gameObject.SetActive(false);
+                        __instance.m_ObjectList[1].transform.GetChild(19).gameObject.SetActive(false);
+                        __instance.m_ObjectList[1].transform.GetChild(20).gameObject.SetActive(false);
+                        __instance.m_ObjectList[1].transform.GetChild(21).gameObject.SetActive(false);
+
+                        __instance.m_ObjectList[2].transform.GetChild(0).gameObject.SetActive(false);
+                        __instance.m_ObjectList[2].transform.GetChild(4).gameObject.SetActive(false);
+                        __instance.m_ObjectList[2].transform.GetChild(6).gameObject.SetActive(false);
+                        __instance.m_ObjectList[2].transform.GetChild(8).gameObject.SetActive(false);
+                        __instance.m_ObjectList[2].transform.GetChild(9).gameObject.SetActive(false);
+                        __instance.m_ObjectList[2].transform.GetChild(10).gameObject.SetActive(false);
+                        __instance.m_ObjectList[2].transform.GetChild(11).gameObject.SetActive(false);
+                        __instance.m_ObjectList[2].transform.GetChild(12).gameObject.SetActive(false);
+                        __instance.m_ObjectList[2].transform.GetChild(13).gameObject.SetActive(false);
+                        __instance.m_ObjectList[2].transform.GetChild(14).gameObject.SetActive(false);
+                        __instance.m_ObjectList[2].transform.GetChild(15).gameObject.SetActive(false);
+                        __instance.m_ObjectList[2].transform.GetChild(16).gameObject.SetActive(false);
+                        __instance.m_ObjectList[2].transform.GetChild(17).gameObject.SetActive(false);
+                        __instance.m_ObjectList[2].transform.GetChild(18).gameObject.SetActive(false);
+                        __instance.m_ObjectList[2].transform.GetChild(19).gameObject.SetActive(false);
+                        __instance.m_ObjectList[2].transform.GetChild(20).gameObject.SetActive(false);
+                        __instance.m_ObjectList[2].transform.GetChild(21).gameObject.SetActive(false);
+                        return false;
+                    }
+                    else if (__instance.m_OldSaveObject != null)
+                    {
+                        if (__instance.m_OldSaveObject.name == "FishHuts_oldsave" && Settings.settings.doorEnabled)
+                        {
+                            if (regionName == "LakeRegion")
+                            {
+                                __instance.m_ObjectList[0].SetActive(true);
+                                __instance.m_ObjectList[1].SetActive(true);
+                                __instance.m_ObjectList[2].SetActive(true);
+
+                                __instance.m_ObjectList[0].transform.GetChild(0).gameObject.SetActive(false);
+                                __instance.m_ObjectList[0].transform.GetChild(1).gameObject.SetActive(false);
+                                __instance.m_ObjectList[0].transform.GetChild(2).gameObject.SetActive(false);
+
+                                __instance.m_ObjectList[1].transform.GetChild(0).gameObject.SetActive(false);
+                                __instance.m_ObjectList[1].transform.GetChild(1).gameObject.SetActive(false);
+                                __instance.m_ObjectList[1].transform.GetChild(2).gameObject.SetActive(false);
+
+                                __instance.m_ObjectList[2].transform.GetChild(0).gameObject.SetActive(false);
+                                __instance.m_ObjectList[2].transform.GetChild(1).gameObject.SetActive(false);
+
+                                return false;
+                            }
+                            else if (regionName == "CoastalRegion")
+                            {
+                                __instance.m_ObjectList[0].SetActive(true);
+                                __instance.m_ObjectList[1].SetActive(true);
+                                __instance.m_ObjectList[2].SetActive(true);
+                                __instance.m_ObjectList[3].SetActive(true);
+
+                                __instance.m_ObjectList[0].transform.GetChild(0).gameObject.SetActive(false);
+                                __instance.m_ObjectList[0].transform.GetChild(1).gameObject.SetActive(false);
+                                __instance.m_ObjectList[0].transform.GetChild(4).gameObject.SetActive(false);
+                                __instance.m_ObjectList[0].transform.GetChild(5).gameObject.SetActive(false);
+                                __instance.m_ObjectList[0].transform.GetChild(6).gameObject.SetActive(false);
+
+                                __instance.m_ObjectList[1].transform.GetChild(0).gameObject.SetActive(false);
+                                __instance.m_ObjectList[1].transform.GetChild(1).gameObject.SetActive(false);
+                                __instance.m_ObjectList[1].transform.GetChild(2).gameObject.SetActive(false);
+                                __instance.m_ObjectList[1].transform.GetChild(3).gameObject.SetActive(false);
+                                __instance.m_ObjectList[1].transform.GetChild(4).gameObject.SetActive(false);
+                                __instance.m_ObjectList[1].transform.GetChild(5).gameObject.SetActive(false);
+                                __instance.m_ObjectList[1].transform.GetChild(6).gameObject.SetActive(false);
+
+                                __instance.m_ObjectList[2].transform.GetChild(0).gameObject.SetActive(false);
+                                __instance.m_ObjectList[2].transform.GetChild(1).gameObject.SetActive(false);
+                                __instance.m_ObjectList[2].transform.GetChild(2).gameObject.SetActive(false);
+                                __instance.m_ObjectList[2].transform.GetChild(3).gameObject.SetActive(false);
+                                __instance.m_ObjectList[2].transform.GetChild(4).gameObject.SetActive(false);
+                                __instance.m_ObjectList[2].transform.GetChild(5).gameObject.SetActive(false);
+                                __instance.m_ObjectList[2].transform.GetChild(6).gameObject.SetActive(false);
+
+                                __instance.m_ObjectList[3].transform.GetChild(0).gameObject.SetActive(false);
+                                __instance.m_ObjectList[3].transform.GetChild(1).gameObject.SetActive(false);
+                                __instance.m_ObjectList[3].transform.GetChild(2).gameObject.SetActive(false);
+                                __instance.m_ObjectList[3].transform.GetChild(3).gameObject.SetActive(false);
+                                __instance.m_ObjectList[3].transform.GetChild(4).gameObject.SetActive(false);
+                                __instance.m_ObjectList[3].transform.GetChild(5).gameObject.SetActive(false);
+                                __instance.m_ObjectList[3].transform.GetChild(7).gameObject.SetActive(false);
+                                __instance.m_ObjectList[3].transform.GetChild(8).gameObject.SetActive(false);
+                                return false;
+                            }
+                        }
+                        else if (regionName == "RuralRegion" && __instance.transform.GetParent().name == "Town" && Settings.settings.buildingEnabled)
+                        {
+                            __instance.m_ObjectList[0].SetActive(true);
+                            __instance.m_ObjectList[1].SetActive(true);
+                            __instance.m_ObjectList[2].SetActive(true);
+
+                            __instance.m_ObjectList[0].transform.GetChild(1).gameObject.SetActive(false);
+                            __instance.m_ObjectList[0].transform.GetChild(2).gameObject.SetActive(false);
+
+                            __instance.m_ObjectList[1].transform.GetChild(0).gameObject.SetActive(false);
+                            __instance.m_ObjectList[1].transform.GetChild(1).gameObject.SetActive(false);
+
+                            __instance.m_ObjectList[2].transform.GetChild(0).gameObject.SetActive(false);
+                            __instance.m_ObjectList[2].transform.GetChild(1).gameObject.SetActive(false);
+                            return false;
+                        }
+                        else if (Settings.settings.buildingEnabled)
+                        {
+                            __instance.m_OldSaveObject.SetActive(true);
+                            return false;
+                        }
+                    }
+                    else if (__instance.transform.GetParent().name.Contains("Bear") && Settings.settings.bearEnabled)
+                    {
+                        // ----------   ALL BEAR CAVE ACTIVE ---------
+                        for (int i = 0; i < 4; i++)
+                        {
+                            __instance.m_NumObjectsToEnableByLevel[i] = __instance.m_ObjectList.Count;
+                        }
+                        return true;
+                    }
+                    else if (__instance.transform.GetParent().name.Contains("Moose") && Settings.settings.mooseEnabled)
+                    {
+                        // ----------   ALL BEAR CAVE ACTIVE ---------
+                        for (int i = 0; i < 4; i++)
+                        {
+                            __instance.m_NumObjectsToEnableByLevel[i] = __instance.m_ObjectList.Count;
+                        }
+                        return true;
+                    }
+                    else if (__instance.transform.GetParent().name.ToUpperInvariant().Contains("SHELTER") && Settings.settings.snowShelterEnabled)
+                    {
+                        // ----------   ALL SNOW SHELTER AND FIRE CAMPS ACTIVE ---------
+                        for (int i = 0; i < 4; i++)
+                        {
+                            __instance.m_NumObjectsToEnableByLevel[i] = __instance.m_ObjectList.Count;
+                        }
+                        return true;
+                    }
+                    else if (__instance.transform.GetParent().name.ToUpperInvariant().Contains("CAMP") && Settings.settings.snowShelterEnabled)
+                    {
+                        for (int i = 0; i < 4; i++)
+                        {
+                            __instance.m_NumObjectsToEnableByLevel[i] = __instance.m_ObjectList.Count;
+                        }
+                        return true;
+                    }
+                    else if (__instance.transform.GetParent().name == "SignalFires" && Settings.settings.signalEnabled)
+                    {
+                        // ----------   BOTH SIGNAL FIRES IN HRV ACTIVE ---------
+                        for (int i = 0; i < 4; i++)
+                        {
+                            __instance.m_NumObjectsToEnableByLevel[i] = __instance.m_ObjectList.Count;
+                        }
+                        return true;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,43 @@
+﻿using ModSettings;
+using System.Reflection;
+
+namespace AllRandomLayoutsAtOnce
+{
+    internal static class Settings
+    {
+        internal static AllRandomLayoutsAtOnceSettings settings = new();
+
+        internal static void OnLoad()
+        {
+            settings.AddToModSettings("All Random Layouts At Once");
+            settings.RefreshGUI();
+        }
+    }
+
+    internal class AllRandomLayoutsAtOnceSettings : JsonModSettings
+    {
+        [Name("Enable all buildings")]
+        [Description("All vanilla lootable building will be active (instead of there burnt version)")]
+        public bool buildingEnabled = true;
+
+        [Name("Enable all fishing hut doors")]
+        [Description("All vanilla door for fishing huts will be active")]
+        public bool doorEnabled = true;
+
+        [Name("Enable all bear caves")]
+        [Description("All bear cave will be active (With bones in it)")]
+        public bool bearEnabled = true;
+
+        [Name("Enable all moose area")]
+        [Description("All moose area will be active (With scratch on trees)")]
+        public bool mooseEnabled = true;
+
+        [Name("Enable all snow shelters camps")]
+        [Description("All snow shelter camps will be active")]
+        public bool snowShelterEnabled = true;
+
+        [Name("Enable all signal fires in HRV")]
+        [Description("Both signal fires in HRV will be active")]
+        public bool signalEnabled = true;
+    }
+}
