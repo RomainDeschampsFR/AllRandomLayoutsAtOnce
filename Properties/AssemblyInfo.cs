75f3b5883d3a84cdd722e2fab083127a6e14823f
romainD <romain.deschamps61@sfr.fr>
2025-01-21 00:34:14 +0100

    Edit mod version (1.2.0)

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -13,9 +13,9 @@ using System.Runtime.InteropServices;
 //Version numbers in C# are a set of 1 to 4 positive integers separated by periods.
 //Mods typically use 3 numbers. For example: 1.2.1
 //The mod version need specified in three places.
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: MelonInfo(typeof(AllRandomLayoutsAtOnce.Implementation), "AllRandomLayoutsAtOnce", "1.0.0", "Romain")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: MelonInfo(typeof(AllRandomLayoutsAtOnce.Implementation), "AllRandomLayoutsAtOnce", "1.2.0", "Romain")]
 
 //This tells MelonLoader that the mod is only for The Long Dark.
 [assembly: MelonGame("Hinterland", "TheLongDark")]
